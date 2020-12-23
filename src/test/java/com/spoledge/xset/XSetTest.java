adff6bad283927cace5bb8e0f6edab1c2ce94a94
Vaclav Bartacek <vbartacek@gmail.com>
2020-12-23 18:16:29 +0100

    Travis to SonarCloud integration
    First fragment of test added.
    JaCoCo configured within maven profile "coverage".
    Travis configured to integrate with SonarCloud - token configured in
    Travis repository settings.
    Changed Travis JDK to OpenJDK 11.

---
+++ src/test/java/com/spoledge/xset/XSetTest.java
@@ -0,0 +1,18 @@
+package com.spoledge.xset;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Test suited for {@link XSet}.
+ *
+ * @author Vaclav Bartacek
+ */
+class XSetTest {
+
+    @Test
+    void testEmpty() {
+        XSet.empty();
+    }
+
+}
